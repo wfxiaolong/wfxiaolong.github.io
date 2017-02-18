fb21ca8b6e3d2f71712410ccba187304de77f66f
linxiaolong <542136758@qq.com>
2017-02-18 14:43:24 +0800

    [Add]daily commit with date Sat Feb 18 14:43:24 CST 2017

--- resume/js/echart.js
+++ resume/js/echart.js
@@ -8,48 +8,86 @@
 
 var TypeChart = echarts.init(document.getElementById('echartId_type'));
 option = {
-    tooltip: {
-        trigger: 'item',
-        formatter: "{a} <br/>{b}: {c} ({d}%)"
+    color: ['#606b77'],
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
     },
-    legend: {
-        orient: 'vertical',
-        x: 'left',
-        data:['iOS','Frontend','Backend','Android']
+    grid: {
+        left: '0%',
+        right: '8%',
+        bottom: '18%',
+        containLabel: true
     },
-    series: [
+    xAxis : [
         {
-            name:'Skill Type',
-            type:'pie',
-            radius: ['30%', '70%'],
-            avoidLabelOverlap: false,
-            label: {
-                normal: {
-                    show: false,
-                    position: 'center'
-                },
-                emphasis: {
-                    show: true,
-                    textStyle: {
-                        fontSize: '15',
-                        fontWeight: 'bold'
-                    }
-                }
-            },
-            labelLine: {
-                normal: {
-                    show: false
-                }
-            },
-            data:[
-                {value:38, name:'iOS'},
-                {value:34, name:'Frontend'},
-                {value:20, name:'Backend'},
-                {value:8, name:'Android'}
-            ]
+            type : 'category',
+            data : ['Frontend', 'iOS', 'Backend', 'Backtage', 'Hybrid', 'Android', 'Ohters'],
+            axisTick: {
+                alignWithLabel: true
+            }
+        }
+    ],
+    yAxis : [
+        {
+            type : 'value'
+        }
+    ],
+    series : [
+        {
+            name:'value',
+            type:'bar',
+            barWidth: '46%',
+            data:[100, 95, 80, 80, 95, 30, 30]
         }
     ]
 };
+
+// option = {
+//     tooltip: {
+//         trigger: 'item',
+//         formatter: "{a} <br/>{b}: {c} ({d}%)"
+//     },
+//     legend: {
+//         orient: 'vertical',
+//         x: 'left',
+//         data:['iOS','Frontend','Backend','Android']
+//     },
+//     series: [
+//         {
+//             name:'Skill Type',
+//             type:'pie',
+//             radius: ['30%', '70%'],
+//             avoidLabelOverlap: false,
+//             label: {
+//                 normal: {
+//                     show: false,
+//                     position: 'center'
+//                 },
+//                 emphasis: {
+//                     show: true,
+//                     textStyle: {
+//                         fontSize: '15',
+//                         fontWeight: 'bold'
+//                     }
+//                 }
+//             },
+//             labelLine: {
+//                 normal: {
+//                     show: false
+//                 }
+//             },
+//             data:[
+//                 {value:38, name:'iOS'},
+//                 {value:34, name:'Frontend'},
+//                 {value:20, name:'Backend'},
+//                 {value:8, name:'Android'}
+//             ]
+//         }
+//     ]
+// };
 TypeChart.setOption(option);
 
 
@@ -89,8 +127,8 @@ option = {
                 }
             },
             data:[
-                {value:30, name:'Objective-C'},
-                {value:22, name:'Frontend'},
+                {value:30, name:'Frontend'},
+                {value:22, name:'Objective-C'},
                 {value:22, name:'PHP'},
                 {value:8, name:'Golang'},
                 {value:8, name:'Python'},
